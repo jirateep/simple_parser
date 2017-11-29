83bd80b24e7ae101ed0d9027964ba6f1e850dbc2
jirateep <charoenshop@gmail.com>
2017-11-29 19:20:59 +0700

    call tokenizer

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
+from tokenizer import Tokenizer
+
 sentence = ' '
+tokenizer = Tokenizer()
 while sentence != '' :
 	sentence = input()
+	result = tokenizer.tokenizing()
+	print(result)
--- tokenizer.py
+++ tokenizer.py
@@ -1,4 +1,4 @@
-class tokenizer() :
+class Tokenizer() :
 	def __init__() :
 		self.soul = 
 		{
